0877bc716186cc2e6613423778a544cd70543dc9
Jon Schinella <jschinel@alumni.nmu.edu>
2023-12-18 23:01:40 -0500

    Navigation and bHome Page Structure Complete

---
+++ MAIN_app.js
@@ -0,0 +1,51 @@
+/* Create a function called `$` for selecting an HTML element
+--------------------------------------------------------------------- */
+function $(cssSelector) {
+    return document.querySelector(cssSelector)
+}
+
+/* All HTML elements we need to manipulate (in the order they appear in index.html)
+--------------------------------------------------------------------- */
+const modalMouseArea = $(".MouseExit")
+const modalContainer = $('.modals')
+const aboutMeBox = $('#Aboutme')
+const socialBox = $('#Social')
+const projectsBox = $('#Projects')
+const aboutMeModal = $('#modalAboutMe')
+const socialModal= $('#modalSocial')
+const projectsModal= $('#modalProjects')
+aboutMeModal.style.display = "none";
+socialModal.style.display = "none";
+projectsModal.style.display = "none";
+
+
+/* THIS SECTION IS FOR THE NAVIGATION
+--------------------------------------------------------------------- */
+aboutMeBox.addEventListener('mouseenter', () =>
+{
+    aboutMeModal.style.display = "flex"
+    socialModal.style.display = "none"
+    projectsModal.style.display = "none"
+    modalContainer.style.marginLeft= "0"
+}
+)
+socialBox.addEventListener('mouseenter', () => 
+{
+    aboutMeModal.style.display = "none"
+    socialModal.style.display = "flex"
+    projectsModal.style.display = "none"
+    modalContainer.style.marginLeft= "33.3dvw"
+})
+projectsBox.addEventListener('mouseenter', () => 
+{
+    aboutMeModal.style.display = "none"
+    socialModal.style.display = "none"
+    projectsModal.style.display = "flex"
+    modalContainer.style.marginLeft= "66.6dvw"
+})
+modalMouseArea.addEventListener('mouseleave', () => 
+{
+    aboutMeModal.style.display = "none"
+    socialModal.style.display = "none"
+    projectsModal.style.display = "none"
+})
