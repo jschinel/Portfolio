a0d49b1e04fa362ac0cc1df545f2b75af9c61916
Jon Schinella <jschinel@alumni.nmu.edu>
2023-12-21 09:03:12 -0500

    Working on main styling

--- Games/app.js
+++ Games/app.js
@@ -1,53 +1,172 @@
+
+/* Reset Variables
+--------------------------------------------------------------------- */
+let computerWord=""
+let wrongNum = 0;
+let correctNum=0;
+
 /* Create a function called `$` for selecting an HTML element
 --------------------------------------------------------------------- */
+
+
 function $(cssSelector) {
     return document.querySelector(cssSelector)
 }
 
-/* All HTML elements we need to manipulate (in the order they appear in index.html)
---------------------------------------------------------------------- */
-const modalMouseArea = $(".MouseExit")
-const modalContainer = $('.modals')
-const aboutMeBox = $('#Aboutme')
-const socialBox = $('#Social')
-const projectsBox = $('#Projects')
-const aboutMeModal = $('#modalAboutMe')
-const socialModal= $('#modalSocial')
-const projectsModal= $('#modalProjects')
-aboutMeModal.style.display = "none";
-socialModal.style.display = "none";
-projectsModal.style.display = "none";
+
+/* Create a function called `calcsStat` for selecting random Element within a Range
+-----------------------------------------------------------------------------------------*/
 
 
-/* THIS SECTION IS FOR THE NAVIGATION
---------------------------------------------------------------------- */
-aboutMeBox.addEventListener('mouseenter', () =>
+function calcStat (min,max)
 {
-    aboutMeModal.style.display = "flex"
-    socialModal.style.display = "none"
-    projectsModal.style.display = "none"
-    modalContainer.style.marginLeft= "0"
+   return Math.floor((Math.random() * (max-min))+ min);
+}
+
+
+/* Create a function called `calcsStat` for selecting random Element within a Range
+-----------------------------------------------------------------------------------------*/
+
+
+function grabWord (category)
+{
+    computerWord = category[calcStat(0,christmasChoices.length)]
+}
+
+
+/* All HTML elements we need to manipulate
+--------------------------------------------------------------------- */
+
+
+const head = $("#head")
+const torso = $('#torso')
+const leftArm = $('#leftArm')
+const rightArm = $('#rightArm')
+const leftLeg = $('#leftLeg')
+const rightLeg = $('#rightLeg')
+const rulesContainer = $('.rulesContainer')
+const gameDisplay = $('.game')
+const christmasPlayGame = $('#christmas')
+const rules = $('#rulesButton')
+const keyBoardBtn = $('.keyboard')
+
+
+/* All HTML elements we need to manipulate computers choice section
+--------------------------------------------------------------------- */
+
+
+const letter1 = $("#l1")
+const letter2 = $("#l2")
+const letter3 = $("#l3")
+const letter4 = $("#l4")
+const letter5 = $("#l5")
+const letter6 = $("#l6")
+const letter7 = $("#l7")
+const letter8 = $("#l8")
+const letter9 = $("#l9")
+const letter10 = $("#l10")
+
+computerLetter=
+[
+    letter1,
+    letter2,
+    letter3,
+    letter4,
+    letter5,
+    letter6,
+    letter7,
+    letter8,
+    letter9,
+    letter10,
+]
+
+for(let i = 0 ; i<=computerLetter.length-1; i ++)
+{
+    let tempId = document.getElementById(computerLetter[i].id)
+    tempId.style.display="none"
+    computerLetter[i].innerText="_"
+}
+
+/* THIS SECTION IS FOR THE GETTING RID OF HANGMAN
+--------------------------------------------------------------------- */
+
+
+rulesContainer.style.display = "grid";
+gameDisplay.style.display = "none";
+
+
+
+
+/* THIS SECTION IS FOR THE GETTING RID OF HANGMAN
+--------------------------------------------------------------------- */
+
+
+head.style.display = "none";
+torso.style.display = "none";
+leftArm.style.display = "none";
+rightArm.style.display = "none";
+leftLeg.style.display = "none";
+rightLeg.style.display = "none";
+
+
+/* THIS SECTION IS FOR THE GETTING RID OF HANGMAN AND GRABBING RANDOM WORD FROM CORRESPONDING ARRAY
+--------------------------------------------------------------------------------------------------*/
+
+christmasPlayGame.addEventListener('click', () => 
+{
+    rulesContainer.style.display = "none";
+    gameDisplay.style.display = "grid";
+    for(let i = 0 ; i<=computerLetter.length-1; i ++)
+    {
+        let tempId = document.getElementById(computerLetter[i].id)
+        tempId.style.display="none"
+    }
+    grabWord(christmasChoices);
+    console.clear();
+    for(let i = 0 ; i<=computerWord.length-1 ; i ++)
+    {
+        let tempId = document.getElementById(computerLetter[i].id)
+        tempId.style.display="grid"
+        console.log(computerWord[i]);
+    }
 }
 )
-socialBox.addEventListener('mouseenter', () => 
+rules.addEventListener('click', () => 
 {
-    aboutMeModal.style.display = "none"
-    socialModal.style.display = "flex"
-    projectsModal.style.display = "none"
-    modalContainer.style.marginLeft= "33.3dvw"
-})
-projectsBox.addEventListener('mouseenter', () => 
+    rulesContainer.style.display = "grid";
+    gameDisplay.style.display = "none";
+}
+)
+keyBoardBtn.addEventListener('click', () => 
 {
-    aboutMeModal.style.display = "none"
-    socialModal.style.display = "none"
-    projectsModal.style.display = "flex"
-    modalContainer.style.marginLeft= "66.6dvw"
-})
-modalMouseArea.addEventListener('mouseleave', () => 
-{
-    aboutMeModal.style.display = "none"
-    socialModal.style.display = "none"
-    projectsModal.style.display = "none"
+    correctNum=0;
+    for(let i = 0 ; i < computerWord.length ; i ++)
+    {
+        let tempString = computerWord[i].toUpperCase(i)
+        if(keyBoardBtn.id==tempString)
+        {
+            correctNum=1;
+            computerLetter[i].innerText = keyBoardBtn.id ;
+            console.log("CORRECT")
+        }
+        if(correctNum!=1 && i == computerWord.length-1)
+        {
+            tempBody = hangman[wrongNum]
+            tempBody.style.display="grid";
+            disableBtn = document.querySelector('clicked')
+            // disableBtn.setattribute(disable).value="true"
+            // disableBtn.setattribute(id[0])=keyBoardBtn.id;
+            console.log(disableBtn);
+            wrongNum++
+            correctNum=0;
+            console.log("WRONG")
+        }
+        
+    }
 })
 
 
+/* THIS SECTION CONTAINS THE ARRAY OF COMPUTER CHOICES
+--------------------------------------------------------------------- */
+const christmasChoices= ["santa","presents","coal","snow","sledding"];
+const hangman= [head,torso,leftArm,rightArm,leftLeg,rightLeg];
